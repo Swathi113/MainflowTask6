e29fe004ecb4799e08b3d0aae601c7be036a5c4d
Swathi113 <Swathiepenagandla26@gmail.com>
2024-12-21 18:43:41 +0530

    Add files via upload

---
+++ Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.css';
+
+const Dashboard = () => {
+    return (
+        <div className="dashboard">
+            <h1>Welcome to the Dashboard</h1>
+            <p>Discover a New Way to Experience Apple</p>
+            
+        </div>
+    );
+};
+
+export default Dashboard;
